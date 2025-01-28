21df9a5d9f2238b5fd9986552c89a18396023fed
Vento741 <vento741@mail.ru>
2025-01-28 17:28:46 +0300

    Add portfolio

---
+++ s.py
@@ -0,0 +1,8 @@
+def init(name, patronymic, surname):
+    name = 'Светлана'
+    patronymic = 'Васильевна'
+    surname = 'Дубина'
+
+    return f'{name[0]} {patronymic[0]} {surname[0]}'
+
+print(init())
